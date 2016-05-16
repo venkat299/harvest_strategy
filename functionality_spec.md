aedcd985ba62f1fc3d344716ab20ad50fa02ee51
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2016-05-16 22:21:21 +0530

    added description for module

--- functionality_spec.md
+++ functionality_spec.md
@@ -1,22 +1,28 @@
 # harvest_strategy
 
+* every strategy will analyse the data and recommend `BUY` or `SELL` signals for `stock_x`
+* quantity and order type will be optional and depends upon strategy implementation
+* every strategy should emit `BUY` and  `SELL` signal  for a stock, in whatever order, also, simutanously updating the status of stock to   its collection to (`open`,`closed`).
+* every strategy call for a stock should follow  from `open` to `close` cycle, until it can make an another call for the same stock, unless explicity allowed by the strategy to make an exception
+
+
 ###### #run()
 - [x] should accept data (historical/live) i.e. dt
-- [ ] should check for pending/placed order from db
+- [ ] should check for pending/placed order from db (check `register_order()`)
 - [ ] should query for more info from `harvest_data` 
 - [ ] should apply logic on `dt` based on point 1 and point 2
 - [x] should give `buy` or `sell` signal to `evaluator` after processing data
 
 ###### #register_order()
-- [ ] should accept the order placed information from `evaluator`
+- [ ] should accept the order placed information from `evaluator` and change the status to `open` || `close`.(order status is sent only when the status is changed to `COMPLETE`)
 - [ ] should save the order information to db under its own collection
 
 ###### #watchlist_update()
-- [ ] should add stock to its watchlist
+- [ ] should add stock to its own watchlist collection
 - [ ] should remove stock from its watchlist
 - [ ] should update its db collection accordingly
 
 
 ###### #routine()
-- [ ] should calculate `nrr` aka normalized_rate_of_return, `rate_of_return` for each stock in its watchlist
+- [ ] should calculate `nrr` aka normalized_rate_of_return, `rate_of_return` for each stock in its watchlist. This use case should be implemented irrespective of the strategies
 
--- test/lib/fifty_2_wk.js
+++ test/lib/fifty_2_wk.js
@@ -1,11 +1,11 @@
 var chai = require('chai'),
 	expect = chai.expect,
 	assert = chai.assert,
-	should = chai.should();
+	should = chai.should()
 
-var seneca = require('seneca')();
-seneca.client();
-//seneca.use('../index.js')
+var seneca = require('seneca')()
+seneca.client() // -- integration test
+	//seneca.use('../index.js') // --module test
 
 //=========== mock data ============
 var mock_dt = {
@@ -17,102 +17,121 @@ var mock_dt = {
 	}
 }
 
-var columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume'];
+var columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
 
 var data = mock_dt.data.candles[0].reduce(function(result, field, index) {
-	result[columns[index]] = field;
-	return result;
-}, {});
-//==================================
+		result[columns[index]] = field
+		return result
+	}, {})
+	//==================================
 
 describe('Strategy:fifty_2_wk', function() {
+
+	before('check test server initialization', check_test_server)
+
 	describe('#run -> default test', function() {
 		it('should return an obj {success:true,msg:string}', function(done) {
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
 				default_api_test(err, val, done)
 			})
-		});
-	});
+		})
+	})
 	describe('#run -> buy scenario', function() {
 		it('should return msg with "BUY" call', function(done) {
-			data.close = 125;
+			data.close = 125
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
-				expect(val.cb_msg).to.match(/role:evaluator,cmd:buy/);
-				expect(val.cb_msg_obj).to.be.an('object');
+				expect(val.cb_msg).to.match(/role:evaluator,cmd:buy/)
+				expect(val.cb_msg_obj).to.be.an('object')
 				default_api_test(err, val, done)
 			})
-		});
-	});
-	describe('#run -> buy scenario; check if msg info is proper', function() {
+		})
+	})
+	describe('#run -> buy scenario check if msg info is proper', function() {
 		it('should return msg with "BUY" call with proper object', function(done) {
-			data.close = 125;
+			data.close = 125
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
-				expect(val.cb_msg_obj.symbol).to.be.a('string');
-				expect(val.cb_msg_obj.strategy_id).to.be.a('string');
-				expect(val.cb_msg_obj.track_id).to.be.a('string');
+				expect(val.cb_msg_obj.tradingsymbol).to.be.a('string')
+				expect(val.cb_msg_obj.strategy_id).to.be.a('string')
+				expect(val.cb_msg_obj.track_id).to.be.a('string')
+				expect(val.cb_msg_obj.ltp).to.be.a('number')
 				default_api_test(err, val, done)
 			})
-		});
-	});
+		})
+	})
 	describe('#run -> sell scenario', function() {
 		it('should return msg with "SELL" call', function(done) {
-			data.close = 140;
+			data.close = 140
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
-				expect(val.cb_msg).to.match(/role:evaluator,cmd:sell/);
-				expect(val.cb_msg_obj).to.be.an('object');
+				expect(val.cb_msg).to.match(/role:evaluator,cmd:sell/)
+				expect(val.cb_msg_obj).to.be.an('object')
 				default_api_test(err, val, done)
 			})
-		});
-	});
-	describe('#run -> sell scenario; check if msg info is proper', function() {
+		})
+	})
+	describe('#run -> sell scenario check if msg info is proper', function() {
 		it('should return msg with "SELL" call with proper object', function(done) {
-			data.close = 140;
+			data.close = 140
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
-				expect(val.cb_msg_obj.symbol).to.be.a('string');
-				expect(val.cb_msg_obj.strategy_id).to.be.a('string');
-				expect(val.cb_msg_obj.track_id).to.be.a('string');
+				expect(val.cb_msg_obj.tradingsymbol).to.be.a('string')
+				expect(val.cb_msg_obj.strategy_id).to.be.a('string')
+				expect(val.cb_msg_obj.track_id).to.be.a('string')
 				default_api_test(err, val, done)
 			})
-		});
-	});
+		})
+	})
 	describe('#run -> no call scenario', function() {
 		it('should return msg with "Ignore/No" call', function(done) {
-			data.close = 130;
+			data.close = 130
 			seneca.act('role:strategy,id:fifty_2_wk,cmd:run', {
-				symbol: 'YESBANK',
+				tradingsymbol: 'YESBANK',
 				data: data
 			}, function(err, val) {
-				expect(val.cb_msg).to.match(/role:null,cmd:null/);
-				expect(val.cb_msg_obj).to.be.null;
+				expect(val.cb_msg).to.match(/role:null,cmd:null/)
+				expect(val.cb_msg_obj).to.be.null
 				default_api_test(err, val, done)
 			})
-		});
-	});
-});
+		})
+	})
+})
 
 var default_api_test = function(err, val, cb) {
-	should.not.exist(err);
-	should.exist(val);
-	expect(val).to.be.an('object');
-	expect(val.success).to.be.true;
-	expect(val.cb_msg).to.exist;
-	//expect(val.cb_msg_obj).to.exist;
-	expect(val.curr_track_id).to.exist;
-	expect(val.prev_track_id).to.have.property;
-	cb();
+	should.not.exist(err)
+	should.exist(val)
+	expect(val).to.be.an('object')
+	expect(val.success).to.be.true
+	expect(val.cb_msg).to.exist
+	//expect(val.cb_msg_obj).to.exist
+	expect(val.curr_track_id).to.exist
+	expect(val.prev_track_id).to.have.property
+	cb()
+}
+
+function check_test_server (done) {
+
+	seneca.act('role:test_server,cmd:check_status', function(err, val) {
+		if (err) {
+			//console.log(err)
+			done(err)
+		}
+		expect(val).to.be.an('object')
+		expect(val.success).to.be.true
+		done()
+	})
+	
+
 }
